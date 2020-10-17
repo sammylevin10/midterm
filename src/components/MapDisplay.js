9e75d0f6140956b223e6273bc545bcad8e7d7b00
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-10-17 15:15:31 -0400

    Added functional Google Map inside container div. Created environmental variables for API keys

--- src/components/MapDisplay.js
+++ src/components/MapDisplay.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MapContainer from "../components/MapContainer";
 
 function MapDisplay() {
   return (
     <div className="MapDisplay">
       <h1>Good morning, Brooklyn</h1>
-      <div className="Map-Interface"></div>
+      <div className="Map-Interface">
+        <MapContainer />
+      </div>
     </div>
   );
 }
