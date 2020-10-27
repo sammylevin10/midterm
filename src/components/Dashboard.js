3e1df7007cee2a632214e78654788b3ac53f70bc
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-10-27 15:30:05 -0400

    Successfully read csv data using papaparse

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import DataBlock from "../components/DataBlock";
 import Geocode from "react-geocode";
+import { readString } from "react-papaparse";
+import MaskData from "../components/DataBlock";
 
 function Dashboard({ location }) {
   const [fireData, setFireData] = useState(null);
   const [fireLikelihood, setFireLikelihood] = useState("Loading...");
   const [center, setCenter] = useState(null);
   const [zip, setZip] = useState(null);
+  const [maskData, setMaskData] = useState("Loading...");
 
   useEffect(() => {
     if (location) {
@@ -56,7 +59,19 @@ function Dashboard({ location }) {
 
   useEffect(() => {
     if (zip != null) {
-      console.log("ZIP", zip);
+      const test_csv = `ZIP,NEVER,RARELY,SOMETIMES,FREQUENTLY,ALWAYS
+      11201,0.035,0.034,0.058,0.141,0.732
+      36051,0.053,0.074,0.134,0.295,0.444
+      36578,0.083,0.059,0.098,0.323,0.436`;
+      const read_csv = readString(test_csv);
+      for (var i = 1; i < read_csv.data.length; i++) {
+        if (read_csv.data[i][0].includes(zip)) {
+          let probability = 100 * (1 - read_csv.data[i][5]);
+          let percentage = probability.toString() + "%";
+          setMaskData(percentage);
+        }
+        // console.log(read_csv.data[i][0]);
+      }
     }
   }, [zip]);
 
@@ -64,7 +79,12 @@ function Dashboard({ location }) {
     <div className="Dashboard">
       <h1>Hmmmm... might be best to stay inside today.</h1>
       <div className="DataBlocks">
-        <DataBlock text={"Probability of unmasked encounter"} data={1} />
+        {maskData !== null && (
+          <DataBlock
+            text={"Probability of unmasked encounter"}
+            data={maskData}
+          />
+        )}
         <DataBlock text={"Probability of COVID encounter"} data={2} />
         {fireLikelihood !== null && (
           <DataBlock
